59402043c305f06ea9f87868bb7f0deda1eacd02
Mansaa Kohli <mansaakohli15@gmail.com>
2026-01-28 23:25:44 +0530

    Added basic sudoku solver

---
+++ solver.py
@@ -0,0 +1,23 @@
+def is_valid(board, row, col, num):
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+        if board[i][col] == num:
+            return False
+        if board[(row//3)*3 + i//3][(col//3)*3 + i%3] == num:
+            return False
+    return True
+
+
+def solve(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
